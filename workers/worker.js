794821ab2809dec5ff925c47ab2d4d28b120e3b0
Kurt DaCosta <kurtbradd@gmail.com>
2014-05-14 17:01:33 -0400

    cleaned up worker process

--- workers/worker.js
+++ workers/worker.js
@@ -1,36 +1,30 @@
 var cluster = require('cluster')
-
 var clusterWorkerSize = require('os').cpus().length;
-var pdf;
 
 if (cluster.isMaster) {
 	console.log('Master Worker PID: '+ process.pid);
 	for (var i = 0; i < clusterWorkerSize; i++) {
-		//fork worker processess
 		cluster.fork();
 	}
-} else {
-	console.log('Cluster Worker PID: '+ process.pid);
-	//load pdf module in cluster worker
-	pdf = require('./renderPDF.js')
+}
 
+else {
+
+	console.log('Cluster Worker PID: '+ process.pid);
+	var pdf = require('./renderPDF.js')
 	var	kue = require('kue'),
 	jobs = kue.createQueue();
 
-	//connect to mongodb
 	var mongoose = require('mongoose')
 	mongoose.connect('mongodb://localhost:27017/woof-web');
 	var ScreenedUser = require('../models/screenedUser.js');
 
-	//process renderPDF jobs
+
 	jobs.process('renderPDF', function (job, done){
-		console.log('cluster: '+cluster.worker.id + ' got job' + job.type);
-		console.log('creating PDF for' + job.data.email);
+		//console.log('cluster: '+cluster.worker.id + ' got job' + job.type);
 		pdf.renderPrescreenProfile(job.data.id, function (err, filePath) {
-			console.log('render returned and saving user db now');
 			if (err) {
-				console.log(err);
-				return done && done(err);
+				done && done(err);
 			} else {
 				ScreenedUser.findById(job.data.id, function(err, user) {
 					if (err) {
@@ -63,9 +57,9 @@ if (cluster.isMaster) {
 
 
 // //queue graceful shutdown
-// process.once( 'SIGINT', function ( sig ) {
-//   jobs.shutdown(function(err) {
-// 	console.log( 'Kue is shut down.', err||'' );
-// 	process.exit( 0 );
-//   }, 5000 );
-// });
+process.once( 'SIGINT', function ( sig ) {
+  jobs.shutdown(function(err) {
+		console.log( 'Kue is shut down.', err||'' );
+		process.exit( 0 );
+  }, 5000 );
+});
