73af15b08b2cca98bcae4bb69ada627d708d55d1
Kurt DaCosta <kurtbradd@gmail.com>
2014-05-14 15:14:09 -0400

    new renderPDF algo

--- workers/renderPDF.js
+++ workers/renderPDF.js
@@ -1,49 +1,59 @@
 var phantom = require('phantom');
+var webshot = require('webshot');
 
-var session;
-phantom.create(function (cb_session) {
-	session = cb_session;
-	console.log('session created');
-});
+// var session;
+// phantom.create(function (cb_session) {
+// 	session = cb_session;
+// 	console.log('session created');
+// });
 
-exports.renderPrescreenProfile = function (userID, cb) {
-	console.log('renderPDF got job for id ' + userID);
-	session.createPage(function (page, err) {
-		if (err) {
-			return cb('createpage error');
-		}
-		page.set('paperSize', {format:'A4'});
-		url = 'http:localhost:3000/api/prescreen-profile/render/' + userID;
-		pathToSave = 'userID' + '.pdf';
-		page.open(url, function (status) {
-			if (status === "success") {
-				page.render(pathToSave, function() {
-					console.log("renderd");
-					cb(false, pathToSave);
-				});
-			} else {
-				cb('cant open page error');
-			}
-			page.close();
-			//session.exit();
-		});
-	});
-};
+renderPrescreenProfile();
+
+function renderPrescreenProfile (userID, cb) {
+
+var options = {
+	  screenSize: {
+	    width: 900,
+	    height: 900
+	  }, 
+	  shotSize: {
+	    width: 'all',
+	    height: 'all'
+	  }
+	}
+	url = "http://google.com";
+	webshot(url, 'flickr.pdf', options, function(err) {
+  	console.log(err);
+	});	
+}
+
+
+// exports.renderPrescreenProfile = function (userID, cb) {
+// 	console.log('renderPDF got job for id ' + userID);
+// 	session.createPage(function (page, err) {
+// 		if (err) {
+// 			return cb('createpage error');
+// 		}
+// 		page.set('paperSize', {format:'A4'});
+// 		url = 'http:localhost:3000/api/prescreen-profile/render/' + userID;
+// 		pathToSave = 'userID' + '.pdf';
+// 		page.open(url, function (status) {
+// 			if (status === "success") {
+// 				page.render(pathToSave, function() {
+// 					console.log("renderd");
+// 					cb(false, pathToSave);
+// 				});
+// 			} else {
+// 				cb('cant open page error');
+// 			}
+// 			page.close();
+// 			//session.exit();
+// 		});
+// 	});
+// };
 
 process.once( 'SIGINT', function ( sig ) {
 	console.log('SIGINT Recieved');
-	session.exit();
+	// session.exit();
   process.exit( 0 );
 });
-
-// var PDFDocument = require('pdfkit');
-// var doc = new PDFDocument;
-// doc.pipe(fs.createWriteStream('output.pdf'));
-// doc.fontSize(25).text('Some text with an embedded font!', 100, 100);
-// doc.addPage().fontSize(25).text('Here is some vector graphics...', 100, 100);
-// doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill("#FF3300");
-// doc.scale(0.6).translate(470, -380).path('M 250,75 L 323,301 131,161 369,161 177,301 z').fill('red', 'even-odd').restore();
-// doc.addPage().fillColor("blue").text('Here is a link!', 100, 100).underline(100, 100, 160, 27, {
-//   color: "#0000FF"
-// }).link(100, 100, 160, 27, 'http://google.com/');
-// doc.end();
