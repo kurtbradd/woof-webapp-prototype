3500aea3f789a07e91f7da353073c3f0d892c735
Kurt DaCosta <kurtbradd@gmail.com>
2014-05-16 23:18:22 -0400

    added download route to serve profiles for email

--- controllers/routes.js
+++ controllers/routes.js
@@ -11,11 +11,17 @@ exports = module.exports = function(app) {
 		res.send('Working');
 	});
 
-	//Serve Angular Application
+	//Download Prescreen Form
+	app.get('/pre-screen-profile/:filepath', function (req, res) {
+		res.download('./workers/pre-screen-profile/' + req.params.filepath);
+	});
+
+	//Serve Prescreen Form
 	app.get('/buyer-prescreen', function (req, res) {
 		res.sendfile('./public/views/index.html');
 	});
 
+	//Server Homepage
 	app.get('/', function (req, res){
 		res.sendfile('./public/views/landing_page/index-app.html');
 	})
