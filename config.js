2f880db2d50275da929996d9fd4762793fc9468a
Kurt DaCosta <kurtbradd@gmail.com>
2014-05-17 15:15:13 -0400

    added email setting to config.js

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
+var keys = require('./keys.js')
+
 module.exports = {
-	email : {
-		name:'kurt'
+	mailer: {
+	service: "Gmail",
+		auth: {
+				user: "kurtbradd@gmail.com",
+				pass: keys.gmail
+		}
 	},
 	database: {
 		url: 'mongodb://localhost:27017/woof-web'
